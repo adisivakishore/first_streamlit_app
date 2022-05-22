a9895c94a54c52b544660849b6eca98e04e8646f
adisivakishore <106007227+adisivakishore@users.noreply.github.com>
2022-05-22 12:45:26 +0530

    Update firstdemo.py

--- firstdemo.py
+++ firstdemo.py
@@ -5,3 +5,5 @@ streamlit.title('My Parents New Healthy Dinner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
